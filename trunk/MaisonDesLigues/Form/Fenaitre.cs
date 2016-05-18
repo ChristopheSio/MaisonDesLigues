6ea0c1b597d212b8d237692c903419976915fb5d
Christophe Sonntag <christophe@wlcl.net>
2016-05-18 15:37:03 +0200

    first

---
+++ trunk/MaisonDesLigues/Form/Fenaitre.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Globalization;
+
+namespace MaisonDesLigues
+{
+
+    enum TypeInscription { Intervenant, Licencie, Benevole, Aucun };
+
+    public partial class Fenaitre : Form
+    {
+
+        ///
+        /// Env
+        ///
+
+        public Fenaitre()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("fr-FR");
+            try {
+                Modele.seConnecter("", "");
+            }
+            catch(Exception e) {
+                DialogResult res = MessageBox.Show("Une erreur de connexion s'est produite :\n\n" + e.Message, "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+            InitializeComponent();
+            laDate.Text = "Le " + Utilitaire.obtenirMaintenant().ToString("d") + " (date de test)";
+        }
+
+
+        ///
+        /// Arrangement : Inscription
+        ///
+
+        Structures.Inscription.LesNuites InscriptionIntervenantLesNuites;
+
+        private void refreshInscription()
+        {
+            if  (this.rbInscriptionIntervenant.Checked)     switchInscription(TypeInscription.Intervenant);
+            else if (this.rbInscriptionLicencie.Checked)    switchInscription(TypeInscription.Licencie);
+            else if (this.rbInscriptionBenevole.Checked)    switchInscription(TypeInscription.Benevole);
+        }
+
+        private void switchInscription(TypeInscription activeType)
+        {
+            switch (activeType)
+            {
+                case TypeInscription.Intervenant:  loadInscriptionIntervenant(); break;
+                case TypeInscription.Benevole:     loadInscriptionBenevole(); break;
+            }
+            // Afin de laisser les elements se charger
+            this.gbInscriptionIntervenant.Visible = (activeType == TypeInscription.Intervenant);
+            this.gbInscriptionBenevole.Visible = (activeType == TypeInscription.Benevole);
+        }
+
+        
+        private void loadInscriptionIntervenant()
+        {
+            if (cbInscriptionIntervenantAtelier.DataSource == null)
+            {
+                cbInscriptionIntervenantAtelier.DataSource = Modele.ObtenirDonnees("VATELIER01");
+                cbInscriptionIntervenantAtelier.ValueMember = "ID";
+                cbInscriptionIntervenantAtelier.DisplayMember = "LIBELLE";
+            }
+            //
+            gbInscriptionIntervenantNuites.Visible = chbInscriptionIntervenantNuites.Checked;
+            if (chbInscriptionIntervenantNuites.Checked)
+            {
+                if (InscriptionIntervenantLesNuites == null)
+                    InscriptionIntervenantLesNuites = new Structures.Inscription.LesNuites(pInscriptionIntervenantNuites);
+            }
+            else
+            {
+                if (InscriptionIntervenantLesNuites != null)
+                    InscriptionIntervenantLesNuites = null; // Supprime les nuites et appel le destructeur de chaque nuitee
+            }
+            
+
+        }
+
+        private void loadInscriptionBenevole()
+        {
+
+        }
+
+        ///
+        /// Event : Inscription
+        ///
+
+        private void rbInscriptionIntervenant_CheckedChanged(object sender, EventArgs e) {
+            switchInscription(TypeInscription.Intervenant);
+        }
+        private void rbInscriptionLicencie_CheckedChanged(object sender, EventArgs e) {
+            switchInscription(TypeInscription.Licencie);
+        }
+        private void rbInscriptionBenevole_CheckedChanged(object sender, EventArgs e) {
+            switchInscription(TypeInscription.Benevole);
+        }
+        private void btInscriptionQuitter_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Voulez vous vraiment quitter ? ", "Attention", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes) {
+                Close();
+            }
+        }
+
+        ///
+        /// Event : Inscription->Intervenant
+        ///
+
+        private void chbInscriptionIntervenantNuites_CheckedChanged(object sender, EventArgs e) {
+            loadInscriptionIntervenant();
+        }
+    }
+}
---
+++ trunk/MaisonDesLigues/Modele.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;  // bibliothèque pour les expressions régulières
+using MaisonDesLigues;
+
+using System.Configuration;
+
+
+namespace MaisonDesLigues
+{
+    
+
+    static class Modele
+    {
+        //
+        // propriétés membres
+        //
+        private static SqlConnection DataBaseConnection = null;
+
+        /*private SqlCommand UneSqlCommand;
+        private SqlDataAdapter UnSqlDataAdapter;
+        private DataTable UneDataTable;
+        private SqlTransaction UneSqlTransaction;*/
+
+        //
+        // méthodes public static
+        //
+
+        /// <summary>permet de se connecter</summary>
+        /// <param name="leLogin">login utilisateur</param>
+        /// <param name="leMotDePasse">mot de passe utilisateur</param>
+        static public void seConnecter(String leLogin, String leMotDePasse)
+        {
+            // <remarks>on commence par récupérer dans CnString les informations contenues dans le fichier app.config 
+            // pour la connectionString de nom StrConnMdl </remarks>
+            ConnectionStringSettings ConnectionStr = ConfigurationManager.ConnectionStrings["MaConnection"];
+            try {
+                ///<remarks>on va remplacer dans la chaine de connexion les paramètres par le login et le pwd saisis dans les zones de texte. Pour ça on va utiliser la méthode Format de la classe String.</remarks>
+                DataBaseConnection = new SqlConnection(string.Format(ConnectionStr.ConnectionString, leLogin, leMotDePasse));
+                DataBaseConnection.Open();
+            } catch (SqlException e) {
+                throw new Exception("Erreur à la connexion : " + e.Message + "\n\n(info:" + ConnectionStr.ConnectionString + ")");
+            }
+            
+        }
+
+        /// <summary>Méthode permettant de fermer la connexion</summary>
+        static public void seDeconecter()
+        {
+            DataBaseConnection.Close();
+        }
+
+        /// <summary>Méthode permettant de savoir ou en est la connexion</summary>
+        static public bool estConecter()
+        {
+            if (DataBaseConnection == null)
+                return false;
+            else {
+                return DataBaseConnection.State != ConnectionState.Closed;
+            }
+        }
+
+        /// <summary>permet de récupérer le contenu d'une table ou d'une vue.</summary>
+        /// <param name="UneTableOuVue"> nom de la table ou la vue dont on veut récupérer le contenu</param>
+        /// <returns>un objet de type datatable contenant les données récupérées</returns>
+        static public DataTable ObtenirDonnees(String UneTableOuVue)
+        {
+            SqlCommand command = new SqlCommand("select * from "+UneTableOuVue, DataBaseConnection);
+            SqlDataAdapter adaptater = new SqlDataAdapter();
+            adaptater.SelectCommand = command;
+            DataTable datatable = new DataTable();
+            adaptater.Fill(datatable);
+            return datatable;
+        }
+
+
+        /// <summary>Procédure publique qui va appeler la procédure stockée permettant d'inscrire un nouvel intervenant sans nuité</summary>
+        /// <remarks> procédure qui va créer : 1- un enregistrement dans la table participant avec typeParticipant à 'I' en cas d'erreurSQL, appel à la méthode GetMessageSql dont le rôle est d'extraire uniquement le message renvoyé par une procédure ou un trigger SQLSERVER</remarks>
+        /// <param name="Cmd">nom de l'objet command concerné par les paramètres</param>
+        /// <param name="pNom">nom du participant</param>
+        /// <param name="pPrenom">prénom du participant</param>
+        /// <param name="pAdresse1">adresse1 du participant</param>
+        /// <param name="pAdresse2">adresse2 du participant</param>
+        /// <param name="pCp">cp du participant</param>
+        /// <param name="pVille">ville du participant</param>
+        /// <param name="pTel">téléphone du participant</param>
+        /// <param name="pMail">mail du participant</param>
+        /// <param name="pIdAtelier"> Id de l'atelier où interviendra l'intervenant</param>
+        /// <param name="pIdStatut">statut de l'intervenant pour l'atelier : animateur ou intervenant ('ANI' ou 'INT')</param>
+        static public void InscrireIntervenant(String pNom, String pPrenom, String pAdresse1, String pAdresse2, String pCp, String pVille, String pTel, String pMail, Int16 pIdAtelier, String pIdStatut)
+        {
+            String MessageErreur = "";
+            SqlTransaction transaction = DataBaseConnection.BeginTransaction();
+            try {
+                SqlCommand command = new SqlCommand("PSnouvelintervenant", DataBaseConnection);
+                command.CommandType = CommandType.StoredProcedure;  // début de la transaction SqlServer il vaut mieux gérer les transactions dans l'applicatif que dans la bd dans les procédures stockées.
+                command.Transaction = transaction;       
+                // on appelle la procédure ParamCommunsNouveauxParticipants pour charger les paramètres communs aux Participants
+                ParamCommunsNouveauxParticipants(command, pNom, pPrenom, pAdresse1, pAdresse2, pCp, pVille, pTel, pMail);
+                // on complète les paramètres spécifiques à l'intervenant
+                command.Parameters.Add("@ptype", SqlDbType.VarChar).Value = "I";   // "I" pour le type du participant (Intervenant)
+                command.Parameters.Add("@pidatelierintervenant", SqlDbType.Int).Value = pIdAtelier;
+                command.Parameters.Add("@pIdStatut", SqlDbType.VarChar).Value = pIdStatut; 
+                //execution
+                command.ExecuteNonQuery();
+                // fin de la transaction. Si on arrive à ce point, c'est qu'aucune exception n'a été levée
+                transaction.Commit();
+            }
+            catch (SqlException e) {
+                MessageErreur = "Erreur SqlServer \n" + GetMessageSql(e.Message);
+            }
+            catch (Exception e) {
+                MessageErreur = e.Message + "Autre Erreur, les informations n'ont pas été correctement saisies";
+            }
+            finally {
+                if (MessageErreur.Length > 0) {
+                    transaction.Rollback();             // annulation de la transaction
+                    throw new Exception(MessageErreur); // Déclenchement de l'exception
+                }
+            }
+        }
+
+        /// <summary>Procédure publique qui va appeler la procédure stockée permettant d'inscrire un nouvel intervenant qui aura des nuités</summary>
+        static public void InscrireLicencie(String pNom, String pPrenom, String pAdresse1, String pAdresse2, String pCp, String pVille, String pTel, String pMail, Int16 pIdAtelier, String pIdStatut)
+        {
+            String MessageErreur = "";
+            SqlTransaction transaction = DataBaseConnection.BeginTransaction();
+            try {
+                SqlCommand command = new SqlCommand("PSnouvelintervenant", DataBaseConnection);
+                command.CommandType = CommandType.StoredProcedure;  // début de la transaction SqlServer il vaut mieux gérer les transactions dans l'applicatif que dans la bd dans les procédures stockées.
+                command.Transaction = transaction;
+                // on appelle la procédure ParamCommunsNouveauxParticipants pour charger les paramètres communs aux Participants
+                ParamCommunsNouveauxParticipants(command, pNom, pPrenom, pAdresse1, pAdresse2, pCp, pVille, pTel, pMail);
+                // on complète les paramètres spécifiques à l'intervenant
+                command.Parameters.Add("@ptype", SqlDbType.VarChar).Value = "I";   // "I" pour le type du participant (Intervenant)
+                command.Parameters.Add("@pidatelierintervenant", SqlDbType.Int).Value = pIdAtelier;
+                command.Parameters.Add("@pIdStatut", SqlDbType.VarChar).Value = pIdStatut;
+                //execution
+                command.ExecuteNonQuery();
+                // fin de la transaction. Si on arrive à ce point, c'est qu'aucune exception n'a été levée
+                transaction.Commit();
+            }
+            catch (SqlException e) {
+                MessageErreur = "Erreur SqlServer \n" + GetMessageSql(e.Message);
+            }
+            catch (Exception e) {
+                MessageErreur = e.Message + "Autre Erreur, les informations n'ont pas été correctement saisies";
+            }
+            finally {
+                if (MessageErreur.Length > 0) {
+                    transaction.Rollback();             // annulation de la transaction
+                    throw new Exception(MessageErreur); // Déclenchement de l'exception
+                }
+            }
+        }
+
+        /// <summary>Procédure publique qui va appeler la procédure stockée permettant d'inscrire un nouvel intervenant qui aura des nuités</summary>
+        static public void InscrireBenevole(String pNom, String pPrenom, String pAdresse1, String pAdresse2, String pCp, String pVille, String pTel, String pMail, Int16 pIdAtelier, String pIdStatut)
+        {
+            String MessageErreur = "";
+            SqlTransaction transaction = DataBaseConnection.BeginTransaction();
+            try {
+                SqlCommand command = new SqlCommand("PSnouvelintervenant", DataBaseConnection);
+                command.CommandType = CommandType.StoredProcedure;  // début de la transaction SqlServer il vaut mieux gérer les transactions dans l'applicatif que dans la bd dans les procédures stockées.
+                command.Transaction = transaction;
+                // on appelle la procédure ParamCommunsNouveauxParticipants pour charger les paramètres communs aux Participants
+                ParamCommunsNouveauxParticipants(command, pNom, pPrenom, pAdresse1, pAdresse2, pCp, pVille, pTel, pMail);
+                // on complète les paramètres spécifiques à l'intervenant
+                command.Parameters.Add("@ptype", SqlDbType.VarChar).Value = "I";   // "I" pour le type du participant (Intervenant)
+                command.Parameters.Add("@pidatelierintervenant", SqlDbType.Int).Value = pIdAtelier;
+                command.Parameters.Add("@pIdStatut", SqlDbType.VarChar).Value = pIdStatut;
+                //execution
+                command.ExecuteNonQuery();
+                // fin de la transaction. Si on arrive à ce point, c'est qu'aucune exception n'a été levée
+                transaction.Commit();
+            }
+            catch (SqlException e) {
+                MessageErreur = "Erreur SqlServer \n" + GetMessageSql(e.Message);
+            }
+            catch (Exception e) {
+                MessageErreur = e.Message + "Autre Erreur, les informations n'ont pas été correctement saisies";
+            }
+            finally {
+                if (MessageErreur.Length > 0)
+                {
+                    transaction.Rollback();             // annulation de la transaction
+                    throw new Exception(MessageErreur); // Déclenchement de l'exception
+                }
+            }
+        }
+
+        /// <summary>
+        /// fonction permettant de construire un dictionnaire dont l'id est l'id d'une nuité et le contenu une date
+        /// sous la la forme : lundi 7 janvier 2014        /// 
+        /// </summary>
+        /// <returns>un dictionnaire dont l'id est l'id d'une nuité et le contenu une date</returns>
+        static public Dictionary<Int16, String> ObtenirDatesNuitees() {
+            Dictionary<Int16, String> LesDatesARetourner = new Dictionary<Int16, String>();
+            DataTable LesDatesNuitees = ObtenirDonnees("VDATENUITEE02");
+            foreach (DataRow UneLigne in LesDatesNuitees.Rows) {
+                LesDatesARetourner.Add(System.Convert.ToInt16(UneLigne["ID"]), UneLigne["DATEARRIVEENUITEE"].ToString());
+            }
+            return LesDatesARetourner;
+        }
+        
+        /// <summary>procédure qui va se charger d'invoquer la procédure stockée qui ira inscrire un participant de type bénévole</summary>
+        /// <param name="Cmd">nom de l'objet command concerné par les paramètres</param>
+        /// <param name="pNom">nom du participant</param>
+        /// <param name="pPrenom">prénom du participant</param>
+        /// <param name="pAdresse1">adresse1 du participant</param>
+        /// <param name="pAdresse2">adresse2 du participant</param>
+        /// <param name="pCp">cp du participant</param>
+        /// <param name="pVille">ville du participant</param>
+        /// <param name="pTel">téléphone du participant</param>
+        /// <param name="pMail">mail du participant</param>
+        /// <param name="pDateNaissance">mail du bénévole</param>
+        /// <param name="pNumeroLicence">numéro de licence du bénévole ou null</param>
+        /// <param name="pDateBenevolat">collection des id des dates où le bénévole sera présent</param>
+        static public void InscrireBenevole(String pNom, String pPrenom, String pAdresse1, String pAdresse2, String pCp, String pVille, String pTel, String pMail, DateTime pDateNaissance, Int64? pNumeroLicence, Collection<Int16> pDateBenevolat)
+        {
+
+
+        }
+
+
+        //
+        // méthodes private static
+        //
+
+        /// <summary>méthode permettant de renvoyer un message d'erreur provenant de la bd après l'avoir formatté. On ne renvoie que le message, sans code erreur</summary>
+        /// <param name="unMessage">message à formater</param>
+        /// <returns>message formaté à afficher dans l'application</returns>
+        static private String GetMessageSql(String unMessage)
+        {
+            String[] message = Regex.Split(unMessage, "SQLSERVER-");
+            return (Regex.Split(message[1], ":"))[1];
+        }
+
+        /// <summary>méthode privée permettant de valoriser les paramètres d'un objet commmand communs aux licenciés, bénévoles et intervenants</summary>
+        /// <param name="Cmd">nom de l'objet command concerné par les paramètres</param>
+        /// <param name="pNom">nom du participant</param>
+        /// <param name="pPrenom">prénom du participant</param>
+        /// <param name="pAdresse1">adresse1 du participant</param>
+        /// <param name="pAdresse2">adresse2 du participant</param>
+        /// <param name="pCp">cp du participant</param>
+        /// <param name="pVille">ville du participant</param>
+        /// <param name="pTel">téléphone du participant</param>
+        /// <param name="pMail">mail du participant</param>
+        static private void ParamCommunsNouveauxParticipants(SqlCommand Cmd, String pNom, String pPrenom, String pAdresse1, String pAdresse2, String pCp, String pVille, String pTel, String pMail)
+        {
+            Cmd.Parameters.Add("@pNom", SqlDbType.VarChar).Value = pNom;
+            Cmd.Parameters.Add("@pPrenom", SqlDbType.VarChar).Value = pPrenom;
+            Cmd.Parameters.Add("@pAdr1", SqlDbType.VarChar).Value = pAdresse1;
+            Cmd.Parameters.Add("@pAdr2", SqlDbType.VarChar).Value = pAdresse2;
+            Cmd.Parameters.Add("@pCp", SqlDbType.VarChar).Value = pCp;
+            Cmd.Parameters.Add("@pVille", SqlDbType.VarChar).Value = pVille;
+            Cmd.Parameters.Add("@pTel", SqlDbType.VarChar).Value = pTel;
+            Cmd.Parameters.Add("@pMail", SqlDbType.VarChar).Value = pMail;
+        }
+
+    }
+}
